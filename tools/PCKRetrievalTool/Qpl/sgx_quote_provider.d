8b6fdd99d4e38c81f917f2b7f0dc3c767dd0fb0a
ethan841 <ethan841@gmail.com>
2022-01-11 16:46:40 +0900

    update sample

---
+++ tools/PCKRetrievalTool/Qpl/sgx_quote_provider.d
@@ -0,0 +1,7 @@
+../sgx_quote_provider.o: ../sgx_quote_provider.cpp \
+ ../../../../QuoteGeneration/pce_wrapper/inc/sgx_pce.h \
+ /opt/intel/sgxsdk/include/sgx_key.h \
+ /opt/intel/sgxsdk/include/sgx_attributes.h \
+ /opt/intel/sgxsdk/include/sgx_report.h \
+ /opt/intel/sgxsdk/include/sgx_key.h ../inc/sgx_quote_provider.h \
+ ../../../../QuoteGeneration/quote_wrapper/common/inc/sgx_ql_lib_common.h
